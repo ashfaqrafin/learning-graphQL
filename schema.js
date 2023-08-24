8d8713db0d8fed0f1e495eb90aa68e8ad0333eb2
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-08-24 21:06:23 +0600

    crash course completed

--- schema.js
+++ schema.js
@@ -29,8 +29,13 @@ export const typeDefs = `#graphql
     type Mutation{
         addGame(game: AddGameInput!): Game
         deleteGame(id: ID!): [Game]
+        updateGame(id:ID!,edit:EditGameInput!):Game
     }
     input AddGameInput {
+        title: String!,
+        platform: [String!]!
+    }
+    input EditGameInput {
         title: String!,
         platform: [String!]!
     }
