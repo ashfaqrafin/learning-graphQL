0c252004d4dc824220059b6aa4791791a25361ac
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-08-24 16:05:33 +0600

    nothing

--- schema.js
+++ schema.js
@@ -1,18 +1,22 @@
 export const typeDefs = `#graphql
-    type Game{
-        id:ID!
-        title:String!
-        platform:[String!]!
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+        reviews: [Review!]
     }
-    type Review{
-        id:ID!
-        rating:Int!
-        content:String!
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+        author: Author!
+        game: Game!
     }
-    type Author{
-        id:ID!
-        name:String!
-        verified:Boolean!
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+        reviews: [Review!]
     }
     type Query{
         games: [Game]
